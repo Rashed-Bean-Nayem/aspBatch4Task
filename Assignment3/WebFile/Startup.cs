40975a6a6c730417f4f1a763b098de074caf7e98
Rashed <rashedamlan02@gmail.com>
2020-12-08 00:02:58 +0600

    added theme

--- Assignment3/WebFile/Startup.cs
+++ Assignment3/WebFile/Startup.cs
@@ -92,6 +92,12 @@ namespace WebFile
 
             app.UseEndpoints(endpoints =>
             {
+                
+                    endpoints.MapControllerRoute(
+                      name: "areas",
+                      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                    );
+                
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
